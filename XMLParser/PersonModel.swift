1fc31c322c9207055f33306cb6a217e0768d2f3a
Senthil kumar K <nightrider.cbe@gmail.com>
2018-12-19 10:46:55 +0530

    commit

---
+++ XMLParser/PersonModel.swift
@@ -0,0 +1,18 @@
+//
+//  PersonModel.swift
+//  XMLParser
+//
+//  Created by SENTHILKUMAR on 19/12/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Person {
+    
+    var name = String()
+    var position = String()
+}
+
+
--- XMLParser/ViewController.swift
+++ XMLParser/ViewController.swift
@@ -8,13 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,XMLParserDelegate {
+   
+    var persons : [Person] = []
+    var pName : String = String()
+    var name = String()
+    var position = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Read XML Parser From File
+        if let path = Bundle.main.url(forResource: "personInfo", withExtension: "xml") {
+            if let parser = XMLParser(contentsOf: path) {
+                parser.delegate = self
+                parser.parse()
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    // 1
+    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:])
+     {
+        pName = elementName
+        if elementName == "personinfo" {
+            name = String()
+            position = String()
+        }
+    
+    
+     }
+    
+    
+    // 2
+    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName == "personinfo"{
+            
+            let person = Person()
+            person.name = name
+            person.position = position
+            
+            persons.append(person)
+            
+            print(person.position)
+        }
+        
+    }
+    
+    
+    // 3
+    public func parser(_ parser: XMLParser, foundCharacters string: String) {
+       
+        let data = string.trimmingCharacters(in: CharacterSet.whitespaces)
+        
+        if (!data.isEmpty) {
+            
+            if pName == "name" {
+                name += data
+            }else if pName == "position" {
+                position += data
+            }
+            
+        }
+        
     }
 
-
 }
+
 
