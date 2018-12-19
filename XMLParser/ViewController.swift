c1369e30196d49129b09db6503ab7844d76fc143
Senthil kumar K <nightrider.cbe@gmail.com>
2018-12-19 09:17:48 +0530

    Update

---
+++ XMLParser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMLParser
+//
+//  Created by SENTHILKUMAR on 19/12/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
